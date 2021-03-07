20d28fe009e4068d9307be0553bf356b65fc3c01
Georgi Ivanov <g.ivanov@blubito.com>
2021-03-07 16:18:12 +0200

    add server handle for prime number decomposition

--- server/server.go
+++ server/server.go
@@ -24,6 +24,25 @@ func (*server) Calculate(ctx context.Context, req *calculatorpb.CalculateRequest
 	return res, nil
 }
 
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	log.Println("[INFO] PrimeNumberDecomposition function invoked")
+	number := req.PrimeNumber.GetPrimeNumber()
+	var divisor = int32(2)
+
+	for number > 1 {
+		if number%divisor == 0 {
+			stream.Send(&calculatorpb.PrimeNumberResponse{
+				PrimeNumber: divisor,
+			})
+			number = number / divisor
+		} else {
+			divisor++
+			log.Printf("[INFO] Divisor has increased to: %v", divisor)
+		}
+	}
+	return nil
+}
+
 func main() {
 	log.Println("Hello, I'm the calculation server!")
 
